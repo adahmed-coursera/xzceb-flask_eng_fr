ad130091e25b8855b4f42af79fe8974f7913847b
adahmed-coursera <96035511+adahmed-coursera@users.noreply.github.com>
2022-08-29 05:13:21 +0700

    updated test version

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,19 +1,20 @@
-from ..translator import english_to_french, french_to_english
+from translator import englishToFrench, frenchToEnglish
 import unittest
+
 
 class TestEnglishToEnglish(unittest.TestCase):
     def test_null_english(self):
-        assert english_to_french('') == "Unable to validate payload size, the 'text' is empty."
+        self.assertEqual(englishToFrench(''), "Unable to validate payload size, the 'text' is empty.")
 
     def test_string_english(self):
-        assert english_to_french('Hello') == 'Bonjour'
+        self.assertEqual(englishToFrench('Hello'),'Bonjour')
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_null_french(self):
-        assert french_to_english('') == "Unable to validate payload size, the 'text' is empty."
+        self.assertEqual(frenchToEnglish(''), "Unable to validate payload size, the 'text' is empty.")
 
     def test_string_french(self):
-        assert french_to_english('Bonjour') == 'Hello'
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
 
 unittest.main()
